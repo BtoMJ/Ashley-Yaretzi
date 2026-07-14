5948fa956207812a26db0a7c9e248033b24bc6a3
BtoMJ <bto.mena.17@gmail.com>
2026-07-13 18:07:46 -0600

    Responsive para tablets y dispositivos de escritorios

--- src/components/Outfit/Outfit.jsx
+++ src/components/Outfit/Outfit.jsx
@@ -1,4 +1,5 @@
 import dress from "../../assets/vestimenta.png";
+import florTitle from "../../assets/florTitle.png";
 import "./Outfit.css";
 
 function Outfit() {
@@ -6,6 +7,7 @@ function Outfit() {
     <section className="outfit-container">
       <h2> Código de</h2>
       <h3> Vestimenta</h3>
+      <img className="flor-title" src={florTitle} alt="flor" />
       <img className="dress-code-img" src={dress} alt="imagen vestimenta" />
       <h4>Casual Elegante </h4>
       <p>Evita tonos rosa y lila</p>
