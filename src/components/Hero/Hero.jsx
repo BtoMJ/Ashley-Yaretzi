2b6c77b7e7040626368a06f8338d01bf1a8172c0
BtoMJ <bto.mena.17@gmail.com>
2026-07-14 13:29:12 -0600

    Pantalla para cuando quitan el nombre de la familia en el enlace

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,22 +2,30 @@ import ashley from "../../assets/AshlyXV-Hero.png";
 import mariposa from "../../assets/mariposa1.gif";
 import flores from "../../assets/floreshero.png";
 import "./Hero.css";
+import ViewLandscape from "../ViewLandscape/ViewLandscape";
 
 function Hero() {
   return (
-    <section className="hero-container">
-      <img className="butterfly" src={mariposa} alt="mariposa" />
-      <div className="photo-frame">
-        <img src={ashley} alt="foto de Ashley" />
-      </div>
-      <div className="data-ash">
-        <h2>Ashley</h2>
-        <h3>MIS XV AÑOS</h3>
-        <p>Recuerda ingresar a tu invitación desde el enlace proporcionado</p>
-      </div>
-      <img className="flower1-hero" src={flores} alt="flores abajo izquierda" />
-      <img className="flower2-hero" src={flores} alt="flores abajo derecha" />
-    </section>
+    <>
+      <section className="hero-container">
+        <div className="photo-frame">
+          <img src={ashley} alt="foto de Ashley" />
+        </div>
+        <div className="data-ash">
+          <img className="butterfly" src={mariposa} alt="mariposa" />
+          <h2>Ashley</h2>
+          <h3>MIS XV AÑOS</h3>
+          <p>Recuerda ingresar a tu invitación desde el enlace proporcionado</p>
+        </div>
+        <img
+          className="flower1-hero"
+          src={flores}
+          alt="flores abajo izquierda"
+        />
+        <img className="flower2-hero" src={flores} alt="flores abajo derecha" />
+      </section>
+      <ViewLandscape />
+    </>
   );
 }
 
--- src/components/NotFound/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-import "./NotFound.css";
-
-function NotFound() {
-  return <div className="not-fouund-container"></div>;
-}
-
-export default NotFound;
